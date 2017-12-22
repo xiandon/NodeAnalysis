ae1c4137d5d95c1f028049613c299eb182d07c79
xiandon <hy090427@163.com>
2017-12-22 10:23:38 +0800

    数据更新

--- wsn/src/main/java/com/xiandon/wsn/node/SmsAnalysis.java
+++ wsn/src/main/java/com/xiandon/wsn/node/SmsAnalysis.java
@@ -76,8 +76,8 @@ public class SmsAnalysis {
         /**
          * 来源地址，传感器节点编号
          */
-        nodeInfo.setNode_nums(wsn.substring(14, 18) + wsn.substring(28, 30));
-
+//        nodeInfo.setNode_nums(wsn.substring(14, 18) + wsn.substring(28, 30));
+        nodeInfo.setNode_nums(wsn.substring(14, 18));
 
         /**
          * 初始地址，系统板号
